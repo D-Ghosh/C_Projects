f0c4742796ce39dc8d2227a29d1b03c94560f4af
A_Porey <aninditaporey002@gmail.com>
2020-06-13 07:56:43 +0530

    Corrected

--- Chap1_F_d.c
+++ Chap1_F_d.c
@@ -1,11 +1,11 @@
-//program to convert Fahrenheit degrees  temperature into Centigrade degrees.
+//program to convert temperature from Fahrenheit degrees into Centigrade degrees.
 
 #include <stdio.h>
 
 int main()
 {
   float C, F;
-  printf("Enter the temp. in ferenhite\n");
+  printf("Enter the temp. in Fahrenheit \n");
   scanf("%f", &F);
   C = (F-32)*(5/9);     //Formula for converting Fahrenhrit temperature into Centigrade degree
   printf("The entered temp. in celcius is %f\n", C);
